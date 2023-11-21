fe418680dfe68f2f8d3f2ce5650513f05d9b78c8
StenlyAndika <stenly.andika@gmail.com>
2023-11-21 15:39:33 +0700

    ehe

--- script.js
+++ script.js
@@ -1,46 +1,39 @@
 let clickCounter = 0;
+function rotateAndZoom() {
+    const img1 = document.querySelector('.image1');
+    const img2 = document.querySelector('.image2');
+    const img3 = document.querySelector('.image3');
 
-        function rotateAndZoom() {
-            const layer1 = document.querySelector('.layer1');
-            const layer2 = document.querySelector('.layer2');
-            const layer3 = document.querySelector('.layer3');
+    img1.classList.remove('rotate-zoom-img-1');
+    img2.classList.remove('rotate-zoom-img-2');
+    img3.classList.remove('rotate-zoom-img-3');
 
-            layer1.classList.remove('rotate-zoom-layer-1');
-            layer2.classList.remove('rotate-zoom-layer-2');
-            layer3.classList.remove('rotate-zoom-layer-3');
+    clickCounter++;
+    const currentImageNumber = clickCounter % 4;
+    const backgroundImage = `${currentImageNumber + 1}.jpg`;
+    img3.src = backgroundImage;
 
-            clickCounter++;
-            const currentImageNumber = clickCounter % 4;
-            const backgroundImage = `${currentImageNumber + 1}.jpg`;
-            layer3.style.backgroundImage = `url("${backgroundImage}")`;
+    img3.classList.add('rotate-zoom-img-3');
 
-            // Rotate layer3 first
-            layer3.classList.add('rotate-zoom-layer-3');
+    img3.addEventListener('animationend', function() {
+        img3.classList.remove('rotate-zoom-img-3');
+    }, { once: true });
 
-            // After a delay, rotate layer2
-            setTimeout(() => {
-                layer2.style.backgroundImage = `url("${backgroundImage}")`;
-                layer2.classList.add('rotate-zoom-layer-2');
-            }, 200);
+    setTimeout(() => {
+        img2.src = backgroundImage;
+        img2.classList.add('rotate-zoom-img-2');
+    }, 200);
 
-            // After a delay, rotate layer1
-            setTimeout(() => {
-                layer1.style.backgroundImage = `url("${backgroundImage}")`;
-                layer1.classList.add('rotate-zoom-layer-1');
-            }, 200);
+    img2.addEventListener('animationend', function() {
+        img2.classList.remove('rotate-zoom-img-2');
+    }, { once: true });
 
-            // Listen for the end of the animation on layer3
-            layer3.addEventListener('animationend', function() {
-                layer3.classList.remove('rotate-zoom-layer-3');
-            }, { once: true });
+    setTimeout(() => {
+        img1.src = backgroundImage;
+        img1.classList.add('rotate-zoom-img-1');
+    }, 200);
 
-            // Listen for the end of the animation on layer2
-            layer2.addEventListener('animationend', function() {
-                layer2.classList.remove('rotate-zoom-layer-2');
-            }, { once: true });
-
-            // Listen for the end of the animation on layer
-            layer1.addEventListener('animationend', function() {
-                layer1.classList.remove('rotate-zoom-layer-1');
-            }, { once: true });
-        }
+    img1.addEventListener('animationend', function() {
+        img1.classList.remove('rotate-zoom-img-1');
+    }, { once: true });
+}
